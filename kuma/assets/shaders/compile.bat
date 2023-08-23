2f714bf9dce7272d5c496cc36c3ff5ce58387944
qqppcp <cxxqing@qq.com>
2023-08-23 16:54:42 +0800

    buildinshader
    test shader

---
+++ kuma/assets/shaders/compile.bat
@@ -0,0 +1,3 @@
+D:/VulkanSDK/1.3.243.0/Bin/glslangValidator.exe -V buildinshader.vert.glsl -o buildinshader.vert.spv
+D:/VulkanSDK/1.3.243.0/Bin/glslangValidator.exe -V buildinshader.frag.glsl -o buildinshader.frag.spv
+pause
